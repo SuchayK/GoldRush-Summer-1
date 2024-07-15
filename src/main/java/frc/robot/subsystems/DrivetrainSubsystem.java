489d65ef08587b58cce0eb4080de8fcbc9bfc665
SuchayK <suchay.kommisetty@gmail.com>
2024-07-15 12:00:59 -0500

    Created Base FRC Code + Drivetrain Subsystem

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.math.geometry.Pose2d;
+//import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.DriveTrainConstants;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+
+  public static CANSparkMax leftFrontMotor = new CANSparkMax(Constants.DriveTrainConstants.leftFrontCANID,
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+  public static CANSparkMax leftBackMotor = new CANSparkMax(Constants.DriveTrainConstants.leftBackCANID,
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+  public static CANSparkMax rightFrontMotor = new CANSparkMax(Constants.DriveTrainConstants.rightFrontCANID,
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+  public static CANSparkMax rightBackMotor = new CANSparkMax(Constants.DriveTrainConstants.rightBackCANID,
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+
+  RelativeEncoder leftEncoder = leftFrontMotor.getEncoder();
+  RelativeEncoder rightEncoder = rightFrontMotor.getEncoder();
+
+  MotorControllerGroup leftControllerGroup = new MotorControllerGroup(leftFrontMotor, leftBackMotor);
+  MotorControllerGroup rightControllerGroup = new MotorControllerGroup(rightFrontMotor, rightBackMotor);
+
+  /** Creates a new ExampleSubsystem. */
+  public DrivetrainSubsystem() {
+    leftFrontMotor.restoreFactoryDefaults();
+    leftBackMotor.restoreFactoryDefaults();
+    rightFrontMotor.restoreFactoryDefaults();
+    rightBackMotor.restoreFactoryDefaults();
+
+    leftEncoder.setPosition(0);
+    rightEncoder.setPosition(0);
+    // zeros out the motors and encoders - J
+
+    leftBackMotor.follow(leftFrontMotor);
+    rightBackMotor.follow(rightFrontMotor);
+    // gets the back motors to do the same thing as the front motors - J
+
+    rightControllerGroup.setInverted(false);
+    leftControllerGroup.setInverted(true);
+    // inverts the right motors so both sides spin in the same direction - J
+
+  }
+
+  @Override
+  public void periodic() {
+
+    // This method will be called once per scheduler run
+
+  }
+
+}
